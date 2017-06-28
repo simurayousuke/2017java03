3d71e97b1ef7a478639c804cc312b736827d475d
yzz <yzz0427@vip.qq.com>
2017-06-28 14:38:22 +0800

    第三天课堂练习

---
+++ ShopManage/src/Manager.java
@@ -0,0 +1,10 @@
+
+
+public class Manager {
+       /**
+        * 管理员信息
+        */	   
+       public String username = "manager";  //管理员名字
+       public String password = "0000";     //管理员密码
+         
+}
